b87431bffe699e7b5fad1fab3ef9fee0b8163a9a
arif mahmud <mahmudarif1175@gmail.com>
2023-02-04 21:04:04 +0600

    added groovy file

---
+++ script.groovy
@@ -0,0 +1,16 @@
+def buildApp() {
+    echo "building the application..."
+    
+} 
+
+
+def testApp() {
+    echo "Testing the application"
+    
+} 
+
+def deployApp() {
+    echo 'deploying the application...'
+} 
+
+return this
