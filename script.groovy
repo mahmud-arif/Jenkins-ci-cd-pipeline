e869c75aeee57803ac8817e5b7d7faef25a771d0
Mahmud <mahmudarif1175@gmail.com>
2023-12-23 23:29:12 +0600

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,15 +1,32 @@
-def buildJar() {
+// def buildJar() {
+//     echo "building the application..."
+//     sh 'mvn package'
+// } 
+
+// def buildImage() {
+//     echo "building the docker image..."
+//     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+//         sh 'docker build -t mahmudairf/demo-app:jma-2.0 .'
+//         sh "echo $PASS | docker login -u $USER --password-stdin"
+//         sh 'docker push mahmudarif/demo-app:jma-2.0'
+//     }
+// } 
+
+// def deployApp() {
+//     echo 'deploying the application...'
+// } 
+
+// return this
+
+def buildApp() {
     echo "building the application..."
-    sh 'mvn package'
+    
 } 
 
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t mahmudairf/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push mahmudarif/demo-app:jma-2.0'
-    }
+
+def testApp() {
+    echo "Testing the application"
+    
 } 
 
 def deployApp() {
