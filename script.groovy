68705a44a7f0d6580f5bfbbb40761313723b2996
arif mahmud <mahmudarif1175@gmail.com>
2023-12-24 15:15:27 +0600

    fix: test commit in the github

--- script.groovy
+++ script.groovy
@@ -1,32 +1,15 @@
-// def buildJar() {
-//     echo "building the application..."
-//     sh 'mvn package'
-// } 
-
-// def buildImage() {
-//     echo "building the docker image..."
-//     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-//         sh 'docker build -t mahmudairf/demo-app:jma-2.0 .'
-//         sh "echo $PASS | docker login -u $USER --password-stdin"
-//         sh 'docker push mahmudarif/demo-app:jma-2.0'
-//     }
-// } 
-
-// def deployApp() {
-//     echo 'deploying the application...'
-// } 
-
-// return this
-
-def buildApp() {
+def buildJar() {
     echo "building the application..."
-    
+    sh 'mvn package'
 } 
 
-
-def testApp() {
-    echo "Testing the application"
-    
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t mahmudairf/demo-app:jma-2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push mahmudarif/demo-app:jma-2.0'
+    }
 } 
 
 def deployApp() {
@@ -34,3 +17,4 @@ def deployApp() {
 } 
 
 return this
+
